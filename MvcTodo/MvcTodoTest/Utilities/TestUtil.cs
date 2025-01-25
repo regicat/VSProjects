7d37d6913ecd46a97cb3459f7312831a126b6373
regicat <regicat@gmail.com>
2025-01-25 11:30:34 +0900

    ViewModelのテストを実装

---
+++ MvcTodo/MvcTodoTest/Utilities/TestUtil.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MvcTodoTest.Utilities;
+
+public static class TestUtil
+{
+	public static string? GetDisplayName(object? instance, string propertyName)
+	{
+		var property = instance?.GetType()?.GetProperty(propertyName);
+		var displayNameAttribute = property?.GetCustomAttribute<DisplayNameAttribute>();
+		return displayNameAttribute?.DisplayName;
+	}
+
+	public static string? GetFormattedDate(object? instance, string propertyName)
+	{
+		var type = instance?.GetType();
+		var property = type?.GetProperty(propertyName);
+		var attribute = property?.GetCustomAttribute<DisplayFormatAttribute>();
+
+		if (attribute?.DataFormatString == null) return null;
+		var date = property?.GetValue(instance);
+		return date == null ? null : string.Format(attribute.DataFormatString, date);
+	}
+}
---
+++ MvcTodo/MvcTodoTest/ViewModels/TodoViewModelTest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using MvcTodo.ViewModels;
+using MvcTodoTest.Utilities;
+
+namespace MvcTodoTest.ViewModels;
+
+[TestFixture]
+public class TodoViewModelTest
+{
+	[Test]
+	public void ViewModelTest()
+	{
+		var actual = new TodoViewModel(1, "やること１", "", new DateTime(2025,1,31), false);
+		Assert.Multiple(() =>
+		{
+			Assert.That(TestUtil.GetDisplayName(actual, "Title"), Is.EqualTo("タイトル"));
+			Assert.That(TestUtil.GetDisplayName(actual, "LimitDate"), Is.EqualTo("期限"));
+			Assert.That(TestUtil.GetFormattedDate(actual, "LimitDate"), Is.EqualTo("2025/01/31"));
+		});
+	}
+}
